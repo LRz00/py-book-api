8e994e53bad774f5483665722f2f32b00ba2f074
Lara Rodrigues <lararodrigues1@natura.net>
2026-01-12 16:27:59 -0300

    feat: add status enum

--- app/schemas/book.py
+++ app/schemas/book.py
@@ -1,9 +1,12 @@
+from enum import Enum
 from pydantic import BaseModel
-
+class Status(Enum):
+    WANT_TO_READ = "want to read"
+    READING = "reading"
+    READ = "read"
 class BookCreate(BaseModel):
     title: str
     author: str
-    status: str
-
+    status: Status
 class BookResponse(BookCreate):
     id: int
